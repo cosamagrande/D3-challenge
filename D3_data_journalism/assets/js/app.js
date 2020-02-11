32f2f77e6e523161d0ce277883d13d32f2690fec
cosamagrande <54606354+cosamagrande@users.noreply.github.com>
2020-02-11 06:04:40 -0600

    finished genrating axis

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,47 @@
 // @TODO: YOUR CODE HERE!
+
+// Setting margins
+var svgWidth = 690;
+var svgHeight = 500;
+var margin = {top: 20,right: 40,bottom: 60,left: 100};
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// SVG - Generating canvas that contains chart and centering it on canvas
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chart= svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Fetching & parsing data
+d3.csv("assets/data/data.csv")
+  .then(function(table) {
+    table.forEach(function(data) {
+      data.age = +data.age;
+      data.smokes = +data.smokes;
+    });
+
+// scale  and axis functions
+    var x = d3.scaleLinear()
+        .domain([d3.min(table, d => d.age)-1, d3.max(table, d => d.age)])
+        .range([0, width]);
+
+    var y = d3.scaleLinear()
+        .domain([d3.min(table, d => d.smokes)-1, d3.max(table, d => d.smokes)])
+        .range([height, 0]);
+
+    var bottom = d3.axisBottom(x);
+    var left = d3.axisLeft(y);
+
+    // Appending axes to chart
+    chart.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottom);
+
+    chart.append("g")
+         .call(left);
+
+});
